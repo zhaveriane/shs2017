0bbc0734ed14f8a1909d1c8eeb2be339b3ade131
Lisandro Jimenez <jimenezl@mit.edu>
2016-01-04 22:48:50 -0500

    add new challenge

--- coding_challenges.cpp
+++ coding_challenges.cpp
@@ -150,5 +150,134 @@ int factorial(int n){
 
 }
 
+// Check for prime number
+
+//
+//Ignore this:
+#include <iostream>
+using namespace std;
+#define println(x) do { cout << x << endl; } while (0) 
+#define print(x) do { cout << x; } while (0) 
+
+
+int main() {
+    if (isPrime(0) == 1){
+        println("Passed test 1!");
+    } else {
+        print("Test 1 failed: returned ");
+        print(isPrime(0));
+        println(" instead of 1");
+    }
+    
+    if (isPrime(1) == 1){
+        println("Passed test 2!");
+    } else {
+        print("Test 2 failed: returned ");
+        print(isPrime(1));
+        println(" instead of 1");
+    }
+
+    if (isPrime(5) == 120){
+        println("Passed test 3!");
+    } else {
+        print("Test 3 failed: returned ");
+        print(isPrime(5));
+        println(" instead of 130");
+    }
+
+    if (isPrime(10) == 3628800){
+        println("Passed test 4!");
+    } else {
+        print("Test 4 failed: returned ");
+        print(isPrime(10));
+        println(" instead of 3628800");
+    }
+    
+    return 0;
+}
+
+
+
+//Ignore this:
+#include <iostream>
+using namespace std;
+#define println(x) do { cout << x << endl; } while (0) 
+#define print(x) do { cout << x; } while (0) 
+
+
+int main() {
+    if (!isPrime(0)){
+        println("Passed test 1!");
+    } else {
+        print("Test 1 failed: returned ");
+        print(isPrime(0));
+        println(" instead of false");
+    }
+    
+    if (!isPrime(1)){
+        println("Passed test 2!");
+    } else {
+        print("Test 2 failed: returned ");
+        print(isPrime(1));
+        println(" instead of false");
+    }
+    
+    if (isPrime(2)){
+        println("Passed test 3!");
+    } else {
+        print("Test 3 failed: returned ");
+        print(isPrime(2));
+        println(" instead of true");
+    }
+    
+    if (!isPrime(4)){
+        println("Passed test 4!");
+    } else {
+        print("Test 4 failed: returned ");
+        print(isPrime(4));
+        println(" instead of false");
+    }
+    
+    if (isPrime(5)){
+        println("Passed test 5!");
+    } else {
+        print("Test 5 failed: returned ");
+        print(isPrime(5));
+        println(" instead of true");
+    }
+    
+    if (!isPrime(100)){
+        println("Passed test 6!");
+    } else {
+        print("Test 6 failed: returned ");
+        print(isPrime(100));
+        println(" instead of false");
+    }
+    return 0;
+}
+
+
+//   
+//   Compute whether n is prime:
+//
+
+bool isPrime(int n){
+    if (n == 0 || n == 1){
+        return false;
+    }
+
+    if (n == 2) {
+        return 2;
+    }
+
+    for(int i = 2; i <= n/2; i++){
+        if ((n%i) == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
 
 
--- coding_challenges.md
+++ coding_challenges.md
@@ -9,3 +9,7 @@ layout: index
 [Challenge 2](cpp_shell/challenge2)
 
 [Challenge 3](cpp_shell/fibbochallenge)
+
+## More Challenges
+
+[Challenge 4](cpp_shell/challenge4)
