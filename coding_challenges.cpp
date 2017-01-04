0ce7a3511052f2b893c41cc59493705926d1ed3c
Sarah <sarahliu@mit.edu>
2017-01-04 16:23:09 +0900

    final fixes for code challenge 1

--- coding_challenges.cpp
+++ coding_challenges.cpp
@@ -64,17 +64,17 @@ int main() {
     }
     
     if (multiply(5, 2) == 10){
-        println("Passed test 1!");
+        println("Passed test 3!");
     } else {
-        print("Test 1 failed: returned ");
+        print("Test 3 failed: returned ");
         print(add(5, 2));
         println(" instead of 10");
     }
 
     if (multiply(10, 32) == 320){
-        println("Passed test 2!");
+        println("Passed test 4!");
     } else {
-        print("Test 2 failed: returned ");
+        print("Test 4 failed: returned ");
         print(add(10, 32));
         println(" instead of 320");
     }
